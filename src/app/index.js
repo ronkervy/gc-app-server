dfa3cbf4e7fa7b4515f38e84de2a710478fe9ca2
Ron kervy G. Palacay <kervy00@gmail.com>
2024-07-02 11:19:27 +0800

    Update index.js
    fix uploading issue when greater than threshold

--- src/app/index.js
+++ src/app/index.js
@@ -23,7 +23,7 @@ app.use(cors());
 app.use('/static',express.static(path.join(__dirname,'../renderer/main_window/public')));
 app.use(express.static(path.join(__dirname,'..','/renderer/main_window/public')));
 app.use(express.json({ limit : '200mb' }));
-app.use(express.urlencoded({ extended : false,limit : '200mb' }));
+app.use(express.urlencoded({ extended : false,limit : '500gb' }));
 
 app.use('/api/v1',apiRoutes);
 
